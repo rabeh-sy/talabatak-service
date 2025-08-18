9f372fab27040297982b8ee7c4b2c238b656e871
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-08-18 23:42:28 +0300

    split admin from api namespace

--- spec/views/restaurants/new.html.tailwindcss_spec.rb
+++ spec/views/restaurants/new.html.tailwindcss_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe "restaurants/new", type: :view do
   it "renders new restaurant form" do
     render
 
-    assert_select "form[action=?][method=?]", restaurants_path, "post" do
+    assert_select "form[action=?][method=?]", admin_restaurants_path, "post" do
       assert_select "input[name=?]", "restaurant[name]"
 
       assert_select "textarea[name=?]", "restaurant[description]"
