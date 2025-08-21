88cafbc91e8e7fb036ee66456b9b312b4ec8ad18
khaled-badenjki <m.k.badenjki2@gmail.com>
2025-08-21 22:38:19 +0300

    link restaurant to user

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,7 @@ class Restaurant < ApplicationRecord
   has_many :menu_items, inverse_of: :restaurant, dependent: :destroy
   has_many :orders, inverse_of: :restaurant, dependent: :destroy
   has_one_attached :logo
+  belongs_to :user
 
   validates :name, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_142347) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_21_193020) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -68,6 +68,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_142347) do
     t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
   create_table "sessions", force: :cascade do |t|
@@ -92,5 +94,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_142347) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "menu_items", "restaurants"
   add_foreign_key "orders", "restaurants"
+  add_foreign_key "restaurants", "users"
   add_foreign_key "sessions", "users"
 end
